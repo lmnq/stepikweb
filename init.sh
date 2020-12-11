28fe0e9562e7b31260ef80aa00919d7053753720
lmnq <55473426+lmnq@users.noreply.github.com>
2020-12-11 23:37:27 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+gunicorn -b 0.0.0.0:8080 -c etc/gunicorn.conf hello:app
 sudo /etc/init.d/gunicorn restart
