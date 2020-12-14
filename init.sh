03a4f1782ae304d9676cd86b45c869d57d62ccce
lmnq <nuraddinov4101@gmail.com>
2020-12-14 13:47:08 +0600

    djangodb2

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/gunicorn.conf
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn-django.conf
 cd web/ask/
 gunicorn --bind=0.0.0.0:8000 --workers=2 --timeout=15 --log-level=debug ask.wsgi:application
-sudo /etc/init.d/gunicorn restart
