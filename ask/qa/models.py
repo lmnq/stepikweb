4e39f5d4a8167c549721f405910bf26810dd5f99
lmnq <nuraddinov4101@gmail.com>
2020-12-14 14:59:27 +0600

    djangodb3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -12,21 +12,21 @@ class Question(models.Model):
         objects = QuestionManager()
         title = models.CharField(default="", max_length=1024)
         text = models.TextField(default="")
-        added_at = models.DateTimeFiels(auto_now_add=True)
+        added_at = models.DateTimeField(auto_now_add=True)
         rating = models.IntegerField(default=0)
-        author = models.ForeignKey(USER, null=True, on_delete=models.SET_NULL)
+        author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
         likes = models.ManyToManyField(User, related_name='question_like_user')
 
         def __str__(self):
             return self.title
 
         def get_absolute_url(self):
-            return f"/question/{self.id}/"
+            return "/question/{}/".format(self.id)
 
 class Answer(models.Model):
     text = models.TextField(default="")
-    added_at = models.DateTimeFiels(auto_now_add=True)
-    question = models.ForeignKey(Question, null=True, on_delete=SET_NULL)
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
