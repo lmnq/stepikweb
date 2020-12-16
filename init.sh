4c53c5957f8a81bb5d50f3d5534cc92075238a11
lmnq <nuraddinov4101@gmail.com>
2020-12-16 14:35:49 +0600

    init

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-cd web/ask/
-gunicorn --bind=0.0.0.0:8000 --workers=2 --timeout=15 --log-level=debug ask.wsgi:application
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
