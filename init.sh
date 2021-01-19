e1eb7804d7a5de1f4008a0d69fee2341a87f2e58
lmnq <nuraddinov4101@gmail.com>
2021-01-19 16:44:48 +0600

    gg

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepik_web;"
+mysql -uroot -e "grant all privileges on stepik_web.* to 'box'@'localhost' with grant option;"
+~/web/ask/manage.py makemigrations qa
+~/web/ask/manage.py migrate qa
+
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
